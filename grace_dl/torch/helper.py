b824025872cbf1cfe58ab80373096eef5fa95272
Hang Xu <hang.xu@kaust.edu.sa>
2020-09-13 23:38:01 +0300

    update

--- grace_dl/torch/helper.py
+++ grace_dl/torch/helper.py
@@ -1,15 +1,15 @@
 def grace_from_params(params):
+    import horovod.torch as hvd
+    world_size = hvd.size()
     comp = params.get('compressor', 'none')
     mem = params.get('memory', 'none')
     comm = params.get('communicator', 'allreduce')
     if comp == 'dgc':
         from grace_dl.torch.compressor.dgc import DgcCompressor
-        compress_ratio = params.get('compress_ratio', 0.3)
-        compressor = DgcCompressor(compress_ratio)
+        compressor = DgcCompressor(compress_ratio=0.01)
     elif comp == 'efsignsgd':
         from grace_dl.torch.compressor.efsignsgd import EFSignSGDCompressor
-        lr = params.get('lr', 0.3)
-        compressor = EFSignSGDCompressor(lr)
+        compressor = EFSignSGDCompressor(lr=0.1)
     elif comp == 'fp16':
         from grace_dl.torch.compressor.fp16 import FP16Compressor
         compressor = FP16Compressor()
@@ -27,48 +27,43 @@ def grace_from_params(params):
         compressor = PowerSGDCompressor()
     elif comp == 'qsgd':
         from grace_dl.torch.compressor.qsgd import QSGDCompressor
-        quantum_num = params.get('quantum_num', 256)
-        compressor = QSGDCompressor(quantum_num)
+        compressor = QSGDCompressor(quantum_num=64)
     elif comp == 'randomk':
         from grace_dl.torch.compressor.randomk import RandomKCompressor
-        compress_ratio = params.get('compress_ratio', 0.3)
-        compressor = RandomKCompressor(compress_ratio)
+        compressor = RandomKCompressor(compress_ratio=0.01)
     elif comp == 'signsgd':
         from grace_dl.torch.compressor.signsgd import SignSGDCompressor
         compressor = SignSGDCompressor()
     elif comp == 'signum':
         from grace_dl.torch.compressor.signum import SignumCompressor
-        momentum = params.get('momentum', 0.9)
-        compressor = SignumCompressor(momentum)
+        compressor = SignumCompressor(momentum=0.9)
     elif comp == 'terngrad':
         from grace_dl.torch.compressor.terngrad import TernGradCompressor
         compressor = TernGradCompressor()
     elif comp == 'threshold':
         from grace_dl.torch.compressor.threshold import ThresholdCompressor
-        threshold = params.get('threshold', 256)
-        compressor = ThresholdCompressor(threshold)
+        compressor = ThresholdCompressor(threshold=0.01)
     elif comp == 'topk':
         from grace_dl.torch.compressor.topk import TopKCompressor
-        compress_ratio = params.get('compress_ratio', 0.3)
-        compressor = TopKCompressor(compress_ratio)
+        compressor = TopKCompressor(compress_ratio=0.01)
     else:
         raise NotImplementedError(comp)
 
     if mem == 'dgc':
         from grace_dl.torch.memory.dgc import DgcMemory
-        momentum = params.get('momentum', 0.3)
-        gradient_clipping = params.get('gradient_clipping', 0.3)
-        memory = DgcMemory(momentum, gradient_clipping)
+        memory = DgcMemory(momentum=0.9, gradient_clipping=False)
     elif mem == 'none':
         from grace_dl.torch.memory.none import NoneMemory
         memory = NoneMemory()
     elif mem == 'powersgd':
         from grace_dl.torch.memory.powersgd import PowerSGDMemory
-        compress_rank = params.get('compress_rank', 1)
-        memory = PowerSGDMemory(compressor.q_memory, compress_rank)
+        memory = PowerSGDMemory(q_memory=compressor.q_memory, compress_rank=1)
     elif mem == 'residual':
         from grace_dl.torch.memory.residual import ResidualMemory
         memory = ResidualMemory()
+    elif mem == 'efsignsgd':
+        from grace_dl.torch.memory.efsignsgd import EFSignSGDMemory
+        memory = EFSignSGDMemory(lr=0.1)
     else:
         raise NotImplementedError(mem)
 
@@ -77,9 +72,9 @@ def grace_from_params(params):
         return Allreduce(compressor, memory)
     elif comm == 'allgather':
         from grace_dl.torch.communicator.allgather import Allgather
-        return Allgather(compressor, memory, params['world_size'])
+        return Allgather(compressor, memory, world_size)
     elif comm == 'broadcast':
         from grace_dl.torch.communicator.broadcast import Broadcast
-        return Broadcast(compressor, memory, params['world_size'])
+        return Broadcast(compressor, memory, world_size)
     else:
         raise NotImplementedError(comm)
