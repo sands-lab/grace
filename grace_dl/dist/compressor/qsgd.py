0b4776f684ff4af6a29da03d8f14c2b21275ae09
ATIcHAlK <microlexcom@live.com>
2021-07-07 20:52:25 +0800

    add buckets quantization for QSGD

--- grace_dl/dist/compressor/qsgd.py
+++ grace_dl/dist/compressor/qsgd.py
@@ -1,20 +1,30 @@
 import torch
-
 from grace_dl.dist import Compressor
 
 
 class QSGDCompressor(Compressor):
 
-    def __init__(self, quantum_num):
+    def __init__(self, quantum_num, bucket_size=128):
         super().__init__()
         self.quantum_num = quantum_num
+        self.bucket_size = bucket_size
 
     def compress(self, tensor, name):
         shape = tensor.size()
         tensor = tensor.flatten()
-
-        norm = tensor.norm()
         abs_gradient = tensor.abs()
+
+        if tensor.numel() % self.bucket_size != 0:
+            pad_size = self.bucket_size - tensor.numel() % self.bucket_size
+            pad_tensor = torch.cat([tensor, torch.zeros(pad_size, dtype=tensor.dtype, device=tensor.device)])
+        else:
+            pad_tensor = tensor
+        pad_tensor = pad_tensor.view([-1, self.bucket_size])
+        pad_tensor_sqsum = torch.sum(pad_tensor ** 2, dim=1)
+        bucket_norm = torch.sqrt(pad_tensor_sqsum)
+        b = torch.ones([1, self.bucket_size], device=tensor.device)
+        expand_norm = torch.matmul(bucket_norm.view([-1, 1]), b)
+        norm = expand_norm.flatten()[:tensor.numel()]
 
         level_float = self.quantum_num / norm * abs_gradient
         previous_level = level_float.floor()
@@ -25,14 +35,17 @@ class QSGDCompressor(Compressor):
         sign = tensor.sign()
         tensor_compressed = (new_level * sign).type(torch.int16)
         tensor_compressed = tensor_compressed.type(torch.int8 if self.quantum_num < 128 else torch.half)
-        tensor_compressed = tensor_compressed, norm
 
-        return tensor_compressed, shape
+        return (tensor_compressed, bucket_norm), shape
 
-    def decompress(self, tensor_compressed, shape):
-        tensor_compressed, norm = tensor_compressed
-
+    def decompress(self, tensor_compressed, ctx):
+        tensor_compressed, bucket_norm = tensor_compressed
+        shape = ctx
+        b = torch.ones([1, self.bucket_size], device=tensor_compressed.device)
+        expand_norm = torch.matmul(bucket_norm.view([-1, 1]), b)
+        norm = expand_norm.flatten()[:shape.numel()]
         decode_output = tensor_compressed.type(torch.float32)
         tensor_decompressed = norm / self.quantum_num * decode_output
         tensor_decompressed = tensor_decompressed.view(shape)
+
         return tensor_decompressed
